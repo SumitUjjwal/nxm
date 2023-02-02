c8f0111298ff689c5eb0be5c371589fa0ce2e4d2
SumitUjjwal <sumitonboard@gmail.com>
2023-02-02 14:00:40 +0530

    using newToken middleware

--- index.js
+++ index.js
@@ -1,33 +1,36 @@
 const express = require("express");
-const {connect} = require("./config/db");
-const {userRouter} = require("./routes/user.route");
-const {aboutRouter} = require("./routes/about.route");
+const { connect } = require("./config/db");
+const { userRouter } = require("./routes/user.route");
+const { aboutRouter } = require("./routes/about.route");
 const { dataRouter } = require("./routes/userdata.route")
 const { authenticate } = require("./middlewares/authenticate.middleware");
+const { newToken} = require("./middlewares/newToken.middleware");
+const port = process.env.port;
 const cors = require("cors");
-require('dotenv').config() 
+require('dotenv').config()
 
 const app = express();
 app.use(cors());
 
-app.get("/", async(req,res)=>{
+app.get("/", async (req, res) => {
        res.send("Welcome to Sumit Ujjwal Photography");
 })
 
 app.use("/user", userRouter);
 app.use(authenticate);
+app.use(newToken);
 app.use("/about", aboutRouter);
 app.use("/data", dataRouter);
 
-const port = process.env.port
 
-app.listen(port, async ()=>{
+
+app.listen(port, async () => {
        try {
               await connect;
               console.log("Connected to database");
+              console.log(`Listening on port: ${port}`);
        } catch (error) {
               console.log(error);
               console.log("Failed to connect to database");
        }
-       console.log(`Listening on port: ${port}`);
 })
