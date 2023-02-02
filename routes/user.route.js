d43eb0a66e465adfc38dbd9a657f0446146b6cee
SumitUjjwal <sumitonboard@gmail.com>
2023-02-02 13:59:09 +0530

    added logout functionality, generating refresh token, sending token and refresh token to cookies, adding expiresIn for token: 2min and for refresh_token: 7d

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,20 +2,21 @@ const express = require("express");
 const { UserModel } = require("../models/user.model");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const fs = require("fs");
+
 const userRouter = express.Router();
 userRouter.use(express.json());
 
 userRouter.post("/register", async (req, res) => {
-       const { username, email, dob, role, location, password, confirmPassword, phone } = req.body;
-       // console.log(req.body)
+       const { username, email, dob, role, location, password, confirmPassword, phone } = req.body;      
 
        if (password !== confirmPassword) {
-              res.json({"msg":"Password Mismatch"})
+              res.json({ "msg": "Password Mismatch" })
        }
        else {
               const checkExisting = await UserModel.findOne({ $or: [{ email: email }, { phone: phone }] });
               if (checkExisting) {
-                     res.json({"msg":"User already exists Please log in"});
+                     res.json({ "msg": "User already exists Please log in" });
               }
               else {
                      try {
@@ -28,12 +29,12 @@ userRouter.post("/register", async (req, res) => {
                                           await user.save();
                                           console.log(user);
                                           console.log(hash)
-                                          res.json({"msg":"Registered Successfully"});
+                                          res.json({ "msg": "Registered Successfully" });
                                    }
                             });
 
                      } catch (error) {
-                            res.json({"msg":"Error in registration"});
+                            res.json({ "msg": "Error in registration" });
                             console.log(error);
                      }
               }
@@ -45,28 +46,56 @@ userRouter.post("/register", async (req, res) => {
 userRouter.post("/login", async (req, res) => {
        const { email, password } = req.body;
        try {
-       const user = await UserModel.findOne({ email });
-       console.log(user);
-       if (user) {
-              bcrypt.compare(password, user["password"], async (err, result) => {
-                     if (result) {
-                            const token = jwt.sign({ course: 'backend' }, 'masai');
-                            res.json({ "msg": "Logged in Successfully", "token": token, "username": user["username"] });
-                     }
-                     else {
-                            console.log("wrong credentials");
-                            res.json({ "msg": "Invalid credentials" })
-                     }
-              })
-       }
-       else {
-              res.json({ "msg": "Please Enter Credentials"});
-       }
+              const user = await UserModel.findOne({ email });
+              // console.log(user);
+              if (user) {
+                     bcrypt.compare(password, user["password"], async (err, result) => {
+                            if (result) {
+                                   const token = jwt.sign({ name: user.username }, 'masai', { expiresIn: 120000 });
+                                   const refresh_token = jwt.sign({ name: user.username }, 'masai', { expiresIn: "7d" });
+                                   res.cookie("token", token).cookie("refresh_token", refresh_token);
+                                   res.json({ "msg": "Logged in Successfully", "token": token, "refresh_token": refresh_token });
+                            }
+                            else {
+                                   console.log("wrong credentials");
+                                   res.json({ "msg": "Invalid credentials" })
+                            }
+                     })
+              }
+              else {
+                     res.json({ "msg": "Please Enter Credentials" });
+              }
        } catch (error) {
-              res.json("Error in login");
+              res.json({ "msg": "Error in login" });
               console.log(error);
        }
 })
+
+// userRouter.post("/newtoken", async (req, res) => {
+//        const token = req.headers.authorization;
+//        const refresh_token = req.headers.refresh_token;
+//        const blacklisted = JSON.parse(fs.readFileSync("./blacklist.json", "utf8"));
+
+//        if (refresh_token && blacklisted.includes(token)){
+//               const token = jwt.sign({ name: user.username }, 'masai', { expiresIn: 120000 });
+//               const refresh_token = jwt.sign({ name: user.username }, 'masai', { expiresIn: "7d" });
+//               res.cookie("token", token).cookie("refresh_token", refresh_token);
+//               res.json({ "msg": "Logged in Successfully", "token": token, "refresh_token": refresh_token });
+//        }
+// })
+
+userRouter.get("/logout", (req, res) => {
+       const token = req.headers.authorization;
+       const blacklisted = JSON.parse(fs.readFileSync("./blacklist.json", "utf8"));
+
+       if (token) {
+              blacklisted.push(token);
+              fs.writeFileSync("./blacklist.json", JSON.stringify(blacklisted))
+              res.json({ "msg": "Logged out Successfully" });
+       }
+});
+
+
 
 module.exports = {
        userRouter
